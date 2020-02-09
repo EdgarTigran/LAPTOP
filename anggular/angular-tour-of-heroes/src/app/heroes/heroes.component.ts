e850f27033ee0cd95c83e8befbacbcbd736be2b7
EdgarTigran <davtjanne@gmail.com>
2020-02-09 22:44:31 +0400

    new Laptop

--- anggular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ anggular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 import { from } from "rxjs";
 
 @Component({
@@ -9,12 +9,15 @@ import { from } from "rxjs";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero: Hero;
-  constructor() {}
+  heroes: Hero[];
 
-  ngOnInit() {}
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
